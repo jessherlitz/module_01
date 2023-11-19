0c56f0803c6a90eaaac5bfbaa4c6c811c14501b6
Jessica Sampaio-Herlitz <102844641+jessampaio@users.noreply.github.com>
2023-11-19 16:19:01 -0500

    Module 01 - Java

---
+++ StudentInfo.java
@@ -0,0 +1,23 @@
+/** 
+   * Simple program that displays student information.
+   *
+   * Activity 01
+   * @author Jessica Sampaio-Herlitz - CPSC 1210
+   * @version 8/19/2023
+*/
+
+public class StudentInfo {
+
+   /** 
+    *  Prints information about a student.
+    *  @param args Command Line arguments - not used.
+   */
+
+   public static void main(String[] args)
+   {
+      System.out.println("Name: Jessica Sampaio-Herlitz");
+      System.out.println("Previous Computer Courses:");
+      System.out.println("   None.");
+   }
+
+}
